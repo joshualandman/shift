dfc145f356ed6a2a6dc74889dc11a8c26e302aee
unknown <cxm7805@GDD-33.main.ad.rit.edu>
2014-01-25 16:53:08 -0500

    new unity stuff

---
+++ GGJ 2014/Assets/scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject player;
+	Vector3 velocity = Vector3.zero;
+	public float smoothtime = 1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 playerPos = player.transform.position;
+		playerPos.z -= 10;
+
+		transform.position = Vector3.SmoothDamp(transform.position,playerPos,ref velocity,smoothtime);
+	}
+}
---
+++ GGJ 2014/Assets/scripts/CharacterControler.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControler : MonoBehaviour {
+
+	public float maxSpeed = 10f;
+	bool facingRight = true;
+
+	Animator anim;
+
+	//falling
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask ground;
+
+	//jumping
+	public float jumpForce = 0f;
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		//checks if grounded
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, ground);
+		anim.SetBool ("Ground", grounded);
+
+		anim.SetFloat ("vSpeed",rigidbody2D.velocity.y);
+		
+		//movement
+		float move = Input.GetAxis ("Horizontal");
+
+		anim.SetFloat ("Speed", Mathf.Abs(move));
+
+		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+
+		if(move > 0 && !facingRight)
+			Flip ();
+		else if(move < 0 && facingRight)
+			Flip ();
+	}
+
+	void Update()
+	{
+		if(grounded && Input.GetKeyDown(KeyCode.Space))//Input.GetAxis ("Jump") > 0)
+		{
+			anim.SetBool("Ground",false);
+			rigidbody2D.AddForce(new Vector2(0, jumpForce));
+		}
+	}
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *=-1;
+		transform.localScale = theScale;
+	}
+}
