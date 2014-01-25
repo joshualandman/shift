86a2839d766c2d3b9dca4f21d2fe914628925c7d
unknown <jsl9284@GDD-35.main.ad.rit.edu>
2014-01-25 00:34:32 -0500

    Created the menu and some 2d platforming

---
+++ New Unity Project 3/Assets/Main Menu/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public Texture backgroundTexture;
+	public float guiPlacementX;
+	public float guiPlacementY;
+
+
+
+	void OnGUI()
+	{
+		//draws texture
+		GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height), backgroundTexture);
+
+		//draws buttons
+		if(GUI.Button(new Rect(Screen.width * .1f, Screen.height * .5f, Screen.width * .3f, Screen.height * .1f), "Play Game"))
+		{
+			//Debug.Log("Clicked Play Game");
+			Application.LoadLevel("gameJamPrototype");
+		}
+
+		if(GUI.Button(new Rect(Screen.width * .1f, Screen.height * guiPlacementY, Screen.width * .3f, Screen.height * .1f), "Options"))
+		{
+			//Debug.Log("Clicked Options");
+		}
+
+
+
+	}
+}
